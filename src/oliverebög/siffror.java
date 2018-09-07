23d892dd9ba2303cba51311fa8e2f22e05ba81ee
Viktor622 <37398124+Viktor622@users.noreply.github.com>
2018-09-07 12:31:56 +0200

    siffror
    siffror

---
+++ src/oliverebög/siffror.java
@@ -0,0 +1,23 @@
+package oliverebög;
+
+public class siffror {
+	
+public static void main(String[] args) {
+		
+		
+		int[] numbers = {1,2,3,4};
+		int sum = 0;
+		for( int i : numbers) {
+		    sum += i;
+		}
+
+		System.out.println(sum);
+		
+		
+		
+		
+		
+		
+		}
+}
+
