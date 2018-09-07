103f8d5f3cc88f982191bda05b70cd48a04ee693
Viktor622 <37398124+Viktor622@users.noreply.github.com>
2018-09-07 12:50:29 +0200

    Person
    Person

---
+++ src/oliverebög/Person.java
@@ -0,0 +1,23 @@
+package oliverebög;
+
+public class Person {
+
+	String name;
+	int age;
+	
+	public Person(String n, int a) {
+		this.name = n;
+		this.age = a;
+		
+	}
+	
+	public static void main(String[] args) {
+		Person p1 = new Person("Oliver",17);
+		Person p2 = new Person("Jaok",43);
+		
+	}
+	
+	public static void compare(Person p1, Person p2) {
+		
+	}
+}
