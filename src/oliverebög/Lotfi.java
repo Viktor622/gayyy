3ea09f3c874682e47abc4a8bfa9b614daef8ce86
Viktor622 <37398124+Viktor622@users.noreply.github.com>
2018-09-07 12:29:44 +0200

    Ord skit
    ord skit

---
+++ src/oliverebög/Lotfi.java
@@ -0,0 +1,42 @@
+package oliverebög;
+
+
+import java.util.Scanner;
+
+public class Lotfi {
+
+	
+	public static void main(String[] args) {
+        
+        
+        
+        Scanner sc = new Scanner(System.in);  
+     
+        System.out.println("ord 1? : ");
+        String ordEtt;
+        ordEtt = sc.nextLine(); 
+        
+        System.out.println("ord 2? : ");
+        String ordTwo;
+        ordTwo = sc.nextLine();
+        
+        System.out.println("ord 3? : ");
+        String ordTre;
+        ordTre = sc.nextLine();
+        
+       
+        System.out.println();
+        System.out.println(ordTre);
+        System.out.println(ordTwo);
+        System.out.println(ordEtt);
+       
+        
+        
+        sc.close();
+    }
+
+	
+	
+
+
+}
